57423fb5def167446de4bd3ba1beeb65e9366d1b
Anita Smyshlyaeva <101178295+smanitas@users.noreply.github.com>
2024-07-08 06:02:06 +0200

    Update main.py

--- airflow/dags/scripts/main.py
+++ airflow/dags/scripts/main.py
@@ -1,16 +1,3 @@
-from morgan_fingerprint_processor import FingerprintProcessor
-from tanimoto_similarity_processor import SimilarityProcessor
-from config import engine, s3, BUCKET_NAME, INPUT_PREFIX, FINGERPRINTS_PREFIX, SIMILARITIES_PREFIX
-
-
-def main():
-    fingerprint_processor = FingerprintProcessor(engine, s3, BUCKET_NAME, INPUT_PREFIX, FINGERPRINTS_PREFIX)
-    fingerprint_processor.compute_and_store_fingerprints()
-
-    similarity_processor = SimilarityProcessor(engine, s3, BUCKET_NAME, INPUT_PREFIX, SIMILARITIES_PREFIX,
-                                               FINGERPRINTS_PREFIX)
-    similarity_processor.compute_and_store_similarity()
-
 
 if __name__ == '__main__':
     main()
